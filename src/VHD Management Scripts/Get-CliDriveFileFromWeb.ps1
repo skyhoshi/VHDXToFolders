13160e2b46c55780ddd529b4134020d1b059b593
Marcus Kern <skyacem@hotmail.com>
2020-09-19 23:10:55 -0500

    Simplified Username retrieval

--- src/VHD Management Scripts/Get-CliDriveFileFromWeb.ps1
+++ src/VHD Management Scripts/Get-CliDriveFileFromWeb.ps1
@@ -1,23 +1,83 @@
 Write-Host "Loading: Get-CliDriveFileFromWeb" -ForegroundColor Yellow;
+function Get-VirtaulHardDriveListFromGist {
+<#     [CmdletBinding()]
+    param (
+        
+    ) #>
+    
+    begin {
+        $gistId = "aa7a0bd55cf0672c9cc3bc8a04176747";
+        $gistFileName = "VirtualHardDriveList.xml";
+        # $MasterDriveListUrl = "https://gist.githubusercontent.com/skyhoshi/aa7a0bd55cf0672c9cc3bc8a04176747/raw/03495e70583ea32061e1e89fafe7f98b03439eee/VirtualHardDriveList.xml";
+        # $MasterDriveListUrl = "https://gist.githubusercontent.com/skyhoshi/aa7a0bd55cf0672c9cc3bc8a04176747/raw/f0585916e173d6ea77e819d99e9b13717bbc338e/VirtualHardDriveList.xml"; 
+    }
+    
+    process {
+        $urlEndpoint = "gists/$gistId"
+        $GistInfo = ConvertFrom-Json (hub api $urlEndpoint);
+    }
+    
+    end {
+        return $GistInfo.files.$gistFileName.content;   
+    }
+}
+
+function Invoke-SpecificDownload {
+<#     [CmdletBinding()]
+    param (
+        
+    ) #>
+    
+    begin {
+        #$MasterDriveListUrl = "https://gist.githubusercontent.com/skyhoshi/aa7a0bd55cf0672c9cc3bc8a04176747/raw/03495e70583ea32061e1e89fafe7f98b03439eee/VirtualHardDriveList.xml";
+        $MasterDriveListUrl = "https://gist.githubusercontent.com/skyhoshi/aa7a0bd55cf0672c9cc3bc8a04176747/raw/f0585916e173d6ea77e819d99e9b13717bbc338e/VirtualHardDriveList.xml";
+        $DriveWebFile = "C:\Windows\Temp\Drives.xml"; 
+    }
+    
+    process {
+
+        Write-Host "Testing $DriveWebFile" -ForegroundColor green; ;
+        if (!(Test-Path -Path $DriveWebFile)) {
+            Write-Host "Downloading Drive File" -ForegroundColor green; ;
+            Invoke-WebRequest -Uri $MasterDriveListUrl -OutFile $DriveWebFile;
+        }
+        Write-Host "Importing File $DriveWebFile" -ForegroundColor green;
+    }
+    
+    end {
+        Write-Host "Removing File: $DriveWebFile" -ForegroundColor green;
+        Remove-item $DriveWebFile -Force;
+    }
+}
+
+
 function Get-CliDriveFileFromWeb {
     begin {
         
     }
     
     process {
-        $MasterDriveListUrl = "https://gist.githubusercontent.com/skyhoshi/aa7a0bd55cf0672c9cc3bc8a04176747/raw/03495e70583ea32061e1e89fafe7f98b03439eee/VirtualHardDriveList.xml";
-        $MasterDriveListUrl = "https://gist.githubusercontent.com/skyhoshi/aa7a0bd55cf0672c9cc3bc8a04176747/raw/f0585916e173d6ea77e819d99e9b13717bbc338e/VirtualHardDriveList.xml";
-        $DriveWebFile = "C:\Windows\Temp\Drives.xml";
-        Write-Host "Testing $DriveWebFile" -ForegroundColor green;;
-        if (!(Test-Path -Path $DriveWebFile)) {
-            Write-Host "Downloading Drive File" -ForegroundColor green;;
-            Invoke-WebRequest -Uri $MasterDriveListUrl -OutFile $DriveWebFile;
-        }
-        Write-Host "Importing File $DriveWebFile" -ForegroundColor green;
-        $VHDDrivesForImport = Import-Clixml -Path $DriveWebFile;
-        Write-Host "Removing File: $DriveWebFile" -ForegroundColor green;
-        Remove-item $DriveWebFile -Force;
-        $NumberOfDrives =([System.Collections.ArrayList] $VHDDrivesForImport).Count;
+        # $gistId = "aa7a0bd55cf0672c9cc3bc8a04176747";
+        # $gistFileName = "VirtualHardDriveList.xml";
+        # $MasterDriveListUrl = "https://gist.githubusercontent.com/skyhoshi/aa7a0bd55cf0672c9cc3bc8a04176747/raw/03495e70583ea32061e1e89fafe7f98b03439eee/VirtualHardDriveList.xml";
+        # $MasterDriveListUrl = "https://gist.githubusercontent.com/skyhoshi/aa7a0bd55cf0672c9cc3bc8a04176747/raw/f0585916e173d6ea77e819d99e9b13717bbc338e/VirtualHardDriveList.xml";
+        # $urlEndpoint = "gists/$gistId"
+        # $GistInfo = ConvertFrom-Json (hub api $urlEndpoint);
+        # $GistInfo.files.$gistFileName.content
+        # $DriveWebFile = "C:\Windows\Temp\Drives.xml";
+        # Write-Host "Testing $DriveWebFile" -ForegroundColor green; ;
+        # if (!(Test-Path -Path $DriveWebFile)) {
+        #     Write-Host "Downloading Drive File" -ForegroundColor green; ;
+        #     Invoke-WebRequest -Uri $MasterDriveListUrl -OutFile $DriveWebFile;
+        # }
+        # Write-Host "Importing File $DriveWebFile" -ForegroundColor green;
+        # ConvertFrom-StringData
+        # $VHDDrivesForImport = Import-Clixml -Path $DriveWebFile;
+        # Write-Host "Removing File: $DriveWebFile" -ForegroundColor green;
+        # Remove-item $DriveWebFile -Force;        
+
+        $VHDDrivesForImport = Import-Clixml -Path Get-VirtaulHardDriveListFromGist;
+        $NumberOfDrives = ([System.Collections.ArrayList] $VHDDrivesForImport).Count;
         Write-Host "Drives Loaded: $NumberOfDrives" -ForegroundColor green;
     }
     
@@ -26,6 +86,8 @@ function Get-CliDriveFileFromWeb {
     }
 }
 # Get-CliDriveFileFromWeb;
+
+Get-CliDriveFileFromWeb
 
 <#
 $DriveByCategory = $VHDDrivesForImport | Where-Object -Property MountCategory -In -Value "Manditory", [MountCategory]::Manditory; #-EQ -Value [MountCategory]::Manditory;
