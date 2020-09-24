38fa65ce501bb9fd890fdbd6c1dc007b9a96394b
Marcus Kern <skyacem@hotmail.com>
2020-09-24 18:12:36 -0500

    Added process to model scripts and requirements

--- src/Module Variables/Varibles.ps1
+++ src/Module Variables/Varibles.ps1
@@ -1,10 +1,10 @@
-$WindowsAppStorage = "O:\VHDX Storage";
-$BaseVHDXStorage = "D:\VHDX Storage";
-$CompoundVHDXStorage = "D:\VHDX Storage\Compound Drives";
-$ProgramFilesX64 = "D:\VHDX Storage\ProgramFile-Drives";
-$ProgramFilesX86 = "D:\VHDX Storage\ProgramFileX86-Drives";
-$WorkRelated = "D:\VHDX Storage\WorkRelated";
-$SystemUserProfiles = "D:\VHDX Storage\$Env:COMPUTERNAME-Profiles";
+$WindowsAppStorage      = "O:\VHDX Storage";
+$BaseVHDXStorage        = "D:\VHDX Storage";
+$CompoundVHDXStorage    = "D:\VHDX Storage\Compound Drives";
+$ProgramFilesX64        = "D:\VHDX Storage\ProgramFile-Drives";
+$ProgramFilesX86        = "D:\VHDX Storage\ProgramFileX86-Drives";
+$WorkRelated            = "D:\VHDX Storage\WorkRelated";
+$SystemUserProfiles     = "D:\VHDX Storage\$Env:COMPUTERNAME-Profiles";
 
 function Write-DebugVariables {
     
@@ -26,3 +26,4 @@ function Write-DebugVariables {
         
     }
 }
+Write-DebugVariables;
---
+++ src/Test-MasterFunctionList.ps1
@@ -0,0 +1,23 @@
+$EnumNames = $(
+    "MountType",
+    "MountCategory"
+);
+$ClassNames = $(
+    "VirtualDriveToLoad"
+);
+$FunctionNames = $(
+    "Test-IsVHDriveMounted",
+    "Write-DebugVariables",
+    "Get-VirtaulHardDriveListFromGist",
+    #"Get-VHDFromConfig"
+    "Mount-PersonalVHDXDrive"
+);
+$VariableNames = $(
+    "WindowsAppStorage"
+    "BaseVHDXStorage"
+    "CompoundVHDXStorage"
+    "ProgramFilesX64"
+    "ProgramFilesX86"
+    "WorkRelated"
+    "SystemUserProfiles"
+);
